cb11e5892ed0822226cfb680bffe28a14fcaa9f9
Alex Peterson <alexander.peterson89@gmail.com>
2019-03-01 10:38:54 -0500

    redid it

--- cryptme.py
+++ cryptme.py
@@ -7,7 +7,57 @@ import os
 import array
 
 
-class Crypter():
+
+class Decrypter():
+    def __init__(self,full_filepath,pwd,salt="3c0ea47b-32ec-495f-bb88-c41c4c10dd13ad2c338d-4746-4a97-843b-76f5b7b8fe9b1ece25e9-39b0-479e-84d0-f5e7945cf5ac"):
+        self.full_filepath = full_filepath
+        self.salt = salt.encode()
+        try:
+            self.hpwd = self._gethash(pwd)
+        except Exception as e:
+            pwd = ""
+            full_filepath = ""
+            salt = ""
+            self.full_filepath = ""
+            self.salt = ""
+            self.hpwd = ""
+            raise Exception("Failed to hash.")
+        del pwd
+
+    def _gethash(self,pwd):
+        bpwd = pwd.encode()
+        hkey = hashlib.pbkdf2_hmac("sha256",bpwd,self.salt,100000,dklen=32)
+        del bpwd
+        del pwd
+        return hkey
+
+    def _dec(self):
+        infile = open(self.full_filepath,"rb")
+        nonce, tag, ctext = [infile.read(x) for x in (16,16,-1)]
+        cipher = AES.new(self.hpwd,AES.MODE_EAX,nonce)
+        ct_data = cipher.decrypt_and_verify(ctext, tag)
+        ct_data = ct_data.decode()
+        infile.close()
+        return ct_data
+
+    def dec(self):
+        try:
+            return self._dec()
+        except Exception as e:
+            self.full_filepath = ""
+            self.salt = ""
+            self.ct_data = ""
+            self.hpwd = ""
+            raise Exception("Failed to encrypt")
+
+    def destroy_object(self):
+        self.full_filepath = ""
+        self.salt = ""
+        self.ct_data = ""
+        self.hpwd = ""
+
+    
+class Encrypter():
     def __init__(self, full_filepath, pwd, ct_data, salt="3c0ea47b-32ec-495f-bb88-c41c4c10dd13ad2c338d-4746-4a97-843b-76f5b7b8fe9b1ece25e9-39b0-479e-84d0-f5e7945cf5ac"):
         self.full_filepath = full_filepath
         self.salt = salt.encode()
@@ -45,14 +95,6 @@ class Crypter():
         [outf.write(x) for x in (cipher.nonce, tag, ctxt)]
         outf.close()
 
-    def _dec(self):
-        infile = open(self.full_filepath,"rb")
-        nonce, tag, ctext = [infile.read(x) for x in (16,16,-1)]
-        cipher = AES.new(self.hpwd,AES.MODE_EAX,nonce)
-        ct_data = cipher.decrypt_and_verify(ctext, tag)
-        ct_data = ct_data.decode()
-        infile.close()
-        return ct_data
 
     def enc(self):
         try:
@@ -84,23 +126,27 @@ class Crypter():
 
 
 if __name__ == "__main__":
+    print("TEST")
     pwd = getpass.getpass("pw:")
     fname = getpass.getpass("fname")
     data = raw_input("Data:")
-    crypter_object = Crypter(full_filepath="./{}.enc".format(str(fname)), pwd=pwd, ct_data=data)
-    del pwd
     if sys.argv[1] == "enc":
-        crypter_object.enc()
-        crypter_object.destroy_object()
+        encrypter_object = Encrypter(full_filepath="./{}.enc".format(str(fname)), pwd=pwd, ct_data=data)
+        encrypter_object.enc()
+        encrypter_object.destroy_object()
     elif sys.argv[1] == "dec":
-        r = crypter_object.dec()
-        print(r)
-        crypter_object.destroy_object()
+        decrypter_object = Decrypter(full_filepath="./{}.enc".format(str(fname)), pwd=pwd, ct_data=data)
+        res = decrypter_object.dec()
+        print(str(res))
+        decrypter_object.destroy_object()
     elif sys.argv[1] == "both":
-        crypter_object.enc()
-        r = crypter_object.dec()
-        print(r)
-        crypter_object.destroy_object()
+        encrypter_object = Encrypter(full_filepath="./{}.enc".format(str(fname)), pwd=pwd, ct_data=data)
+        encrypter_object.enc()
+        encrypter_object.destroy_object()
+        decrypter_object = Decrypter(full_filepath="./{}.enc".format(str(fname)), pwd=pwd)
+        res = decrypter_object.dec()
+        print(str(res))
+        decrypter_object.destroy_object()
     
         
         
